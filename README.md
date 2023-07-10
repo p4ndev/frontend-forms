dab640eeafe25c1dac4086d7512bdff3ce885915
Gustavo Jaques <gustavo.jaques@triskelle.solutions>
2023-07-10 16:11:14 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# forms
+# AngularTemplateForms
+
+- Example without any type of binding
+- Example with one-way data binding
---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<h1>Welcome to the {{ title }}!!!</h1>`
+})
+export class AppComponent {
+  title = 'angular-template-forms';
+}
